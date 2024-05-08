f254d068de7656918ed622e9cd2b20ad5704cc67
allure <1550220889@qq.com>
2024-05-08 21:36:17 +0800

    feature：项目初始化

---
+++ src/app.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@nestjs/common';
+import puppeteer from 'puppeteer';
+
+let cache = null;
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  async getUniversityData() {
+    if (cache) {
+      return cache;
+    }
+
+    const browser = await puppeteer.launch({
+      headless: true,
+      defaultViewport: {
+        width: 0,
+        height: 0,
+      },
+    });
+
+    const page = await browser.newPage();
+
+    await page.goto('https://www.icourse163.org/university/view/all.htm');
+
+    await page.waitForSelector('.u-usitys');
+
+    const universityList = await page.$eval('.u-usitys', (el) => {
+      return [...el.querySelectorAll('.u-usity')].map((item) => {
+        return {
+          name: item.querySelector('img').alt,
+          img: item.querySelector('img').src,
+          link: item.getAttribute('href'),
+        };
+      });
+    });
+
+    await browser.close();
+
+    cache = universityList;
+
+    return universityList;
+  }
+}
