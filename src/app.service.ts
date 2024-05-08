7dc5e302c35a14bce4551855f44936fdaa1c0c90
allure <1550220889@qq.com>
2024-05-08 21:59:36 +0800

    feature：项目初始化

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import puppeteer from 'puppeteer';
+import { Observable, Subscriber } from 'rxjs';
 
 let cache = null;
 
@@ -14,34 +15,53 @@ export class AppService {
       return cache;
     }
 
-    const browser = await puppeteer.launch({
-      headless: true,
-      defaultViewport: {
-        width: 0,
-        height: 0,
-      },
-    });
-
-    const page = await browser.newPage();
-
-    await page.goto('https://www.icourse163.org/university/view/all.htm');
-
-    await page.waitForSelector('.u-usitys');
-
-    const universityList = await page.$eval('.u-usitys', (el) => {
-      return [...el.querySelectorAll('.u-usity')].map((item) => {
-        return {
-          name: item.querySelector('img').alt,
-          img: item.querySelector('img').src,
-          link: item.getAttribute('href'),
-        };
+    async function getData(observer: Subscriber<Record<string, any>>) {
+      const browser = await puppeteer.launch({
+        headless: true,
+        defaultViewport: {
+          width: 0,
+          height: 0,
+        },
       });
+
+      const page = await browser.newPage();
+
+      await page.goto('https://www.icourse163.org/university/view/all.htm');
+
+      await page.waitForSelector('.u-usitys');
+
+      const universityList: Array<Record<string, any>> = await page.$eval(
+        '.u-usitys',
+        (el) => {
+          return [...el.querySelectorAll('.u-usity')].map((item) => {
+            return {
+              name: item.querySelector('img').alt,
+              img: item.querySelector('img').src,
+              link: item.getAttribute('href'),
+            };
+          });
+        },
+      );
+
+      for (let i = 0; i < universityList.length; i++) {
+        const item = universityList[i];
+        await page.goto('https://www.icourse163.org' + item.link);
+
+        await page.waitForSelector('.m-cnt');
+
+        const content = await page.$eval('.m-cnt p', (el) => el.textContent);
+        item.desc = content;
+
+        observer.next({ data: item });
+      }
+
+      await browser.close();
+
+      cache = universityList;
+    }
+
+    return new Observable((observer) => {
+      getData(observer);
     });
-
-    await browser.close();
-
-    cache = universityList;
-
-    return universityList;
   }
 }
